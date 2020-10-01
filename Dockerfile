bf0fc86a57dc04b493a974d4096f0b60e39e0f32
Rishabh Khanna <rishabhkhanna726@gmail.com>
2020-10-01 10:49:12 +0530

    db setup in docker-compose and dockerfile and settings.py

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,23 @@ FROM python:3.7-alpine
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
+RUN apk add --update --no-cache postgresql-client
+# apk is package manager
+# --update means update the registry before we add it
+# don't store the registry index on our docker file and we do this to minimize
+# the number of extra files and packages that are included in 
+# our docker container
+
+
+# install some temp packages that will run on system while we run our 
+# requirements and remove them after
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+    gcc libc-dev linux-headers postgresql-dev
+
 RUN pip install -r /requirements.txt
+
+# delete temp requirements
+RUN apk del .tmp-build-deps
 
 RUN mkdir /app
 WORKDIR /app
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,16 +3,13 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
 
-        email = 'test@indianadmin.com'
-        password = 'test123'
+        email = "test@indianadmin.com"
+        password = "test123"
         user = get_user_model().objects.create_user(
-            email=email,
-            password=password
-        )
+            email=email, password=password)
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -20,8 +17,8 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized """
 
-        email = 'test@INDIANADMIN.com'
-        user = get_user_model().objects.create_user(email, 'test123')
+        email = "test@INDIANADMIN.com"
+        user = get_user_model().objects.create_user(email, "test123")
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
@@ -31,13 +28,12 @@ class ModelTests(TestCase):
         with self.assertRaises(ValueError):
             # means anything that runs inside this and
             # should raise ValueError and if not that means test fail
-            get_user_model().objects.create_user(None, 'test123')
+            get_user_model().objects.create_user(None, "test123")
 
     def test_create_new_superuser(self):
         """ test creating a new superuser """
         user = get_user_model().objects.create_superuser(
-            'test@indianadmin.com',
-            'test123'
+            "test@indianadmin.com", "test123"
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
